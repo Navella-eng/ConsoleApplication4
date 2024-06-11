4606f06982c0c11a83869d032667752692342045
Navella-eng <dfhybof@mail.ru>
2024-06-11 14:27:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include <iostream>
+
+
+
+int main()
+{
+    int ot;
+    setlocale(0, "");
+    using namespace std;
+    cout << "Вы любите воду?\n 1 - Да\n 2- Нет\n";
+    cin >> ot;
+    if (ot == 1)
+    {
+        cout << "Вы не кот!\n";
+    }
+    else
+    {
+        cout << "Вы кот!\n У вас лапки!";
+    }
+
+}
+
